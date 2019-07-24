7b6bdcd69f130751b96303ba9829748ea4e920b6
Juandavid070 <53203813+Juandavid070@users.noreply.github.com>
2019-07-23 22:52:34 -0500

    Add files via upload

---
+++ WSfinanciamiento/src/java/Webservice/WSfinanciacion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Webservice;
+
+import Model.Socio;
+import Model.SocioDao;
+import java.sql.SQLException;
+import java.util.List;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author PC HOGAR
+ */
+@WebService(serviceName = "WSfinanciacion")
+public class WSfinanciacion {
+  SocioDao dao = new SocioDao();
+    /**
+     * Web service operation
+     * @param cantidad
+     * @return 
+     */
+    @WebMethod(operationName = "obtener")
+    public List<Socio> obtener(double cantidad)  {
+      
+        List datos=dao.obtenerCotizacion(cantidad);        
+        
+       
+        
+        return datos;
+    }
+
+    /**
+     * Web service operation
+     */
+   
+}
